5023610d766d21019c3b1068ed46966402c995b2
Ming Yu <mingyujohn0520@gmail.com>
2022-01-29 21:17:42 +1100

    use mro to check the relationship of classes

---
+++ inherit/mro_check.py
@@ -0,0 +1,28 @@
+class Master():
+    def __init__(self):
+        self.skill = "[old methods]"
+    
+    def make_cake(self):
+        print("use {} to make egg cake".format(self.skill))
+        
+class School():
+    def __init__(self):
+        self.skill = "[new methods]"
+        
+    def make_cake(self):
+        print("use {} to make egg cake".format(self.skill))
+
+# inherit the first one here, School is the first one, so inherit School   
+class Prentice(School, Master):
+    def __init__(self):
+        self.skill = "[unique method]"
+        
+    def make_cake(self):
+        print("use {} to make egg cake".format(self.skill))
+
+# moyu = Prentice()
+# print(moyu.skill)
+# moyu.make_cake()
+
+# check the relationship of the classes
+print(Prentice.__mro__)
