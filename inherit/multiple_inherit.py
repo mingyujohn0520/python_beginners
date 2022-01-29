5a743abeac27e0b0484a09f3571a8e564c99a013
Ming Yu <mingyujohn0520@gmail.com>
2022-01-29 19:02:18 +1100

    script for multiple inherit

---
+++ inherit/multiple_inherit.py
@@ -0,0 +1,21 @@
+class Master():
+    def __init__(self):
+        self.skill = "[old methods]"
+    
+    def make_cake(self):
+        print("use {} to make egg cake".format(self.skill))
+        
+class School():
+    def __init__(self):
+        self.skill = "[new methods]"
+        
+    def make_cake(self):
+        print("use {} to make egg cake".format(self.skill))
+
+# inherit the first one here, School is the first one, so inherit School   
+class Prentice(School, Master):
+    pass
+
+moyu = Prentice()
+print(moyu.skill)
+moyu.make_cake()
