f3a7dced58c6a8119eb7a2067b146989b2d8a725
Ming Yu <mingyujohn0520@gmail.com>
2022-02-04 12:07:40 +1100

    import library fundamental

---
+++ library, package/library_fundamental.py
@@ -0,0 +1,16 @@
+import math
+print(math.sqrt(9))
+
+from math import sqrt
+print(sqrt(9))
+
+from math import *
+print(sqrt(9))
+
+import time as tt
+tt.sleep(5)
+print("hello")
+
+from time import sleep as sl
+sl(5)
+print("hello")
